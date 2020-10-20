519c05d3bb892c6d30021466a946c87bf5308d68
Facundo Khalil Videla Yanzon <facundokhalil@gmail.com>
2020-10-20 15:42:25 -0300

    Se conecto con la api de meli y se realizan las llamadas utilizando recursion

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
+const axios = require('axios');
 
+const TOKEN_ENDPOINT = '/admin/oauth/token';
+const USERS_ENDPOINT = '/users/ALL/classifieds_accounts/search';
+const MELI_ENDPOINT = "https://api.mercadolibre.com";
+
+// File operations
 const getFileName = () => {
     if (process.argv[2]) {
         const fileName = process.argv[2];
@@ -20,6 +26,15 @@ const readFile = (fileName) => {
     }
 }
 
+const createFile = (fileName, data) => {
+    fs.writeFile(fileName, data, err => {
+        if (err) console.log('Error al guardar el archivo: ' + err);
+        console.log('El archivo se guardo con exito');
+    });
+}
+
+
+// API operations
 const getCredentials = () => {
     const credentialsFileName = 'meli_creds_admin.json';
     try {
@@ -32,6 +47,41 @@ const getCredentials = () => {
     }
 }
 
+const getToken = (credentials) => {
+    const ENDPOINT = MELI_ENDPOINT + TOKEN_ENDPOINT;
+    const data = axios.post(ENDPOINT, credentials)
+        .then(res => res.data)
+        .catch(err => console.error(err));
+    
+    return data;
+}
+
+const getAllAccounts = (token, accounts = [], offset = 0, limit = 0) => {
+    const ENDPOINT = MELI_ENDPOINT + USERS_ENDPOINT;
+    const params = {
+        access_token: token.access_token,
+        user_type: 'car_dealer, franchise, real_estate_agency',
+        offset
+    };
+
+    return axios.get(ENDPOINT, { params })
+            .then(res => {
+                const accountsPage = res.data.results;
+                const newOffset = offset + res.data.paging.limit;
+                if(accountsPage.length == 0 || limit == 100){
+                    return accounts;
+                } else {
+                    console.log('Se realiza una llamada con cantidad: ' + accountsPage.length);
+                    return getAllAccounts(token, accounts.concat(accountsPage), newOffset, newOffset);
+                }
+                
+            })
+            .catch(err => console.log(err));
+}
+
+
+
+// MAIN
 const app = () => {
     console.log('Inicia la APP');
     try {
@@ -41,7 +91,21 @@ const app = () => {
         console.log('Cantidad de cuentas: ' + accounts.length);
 
         // API CONNECTION
+        let allAccounts = [];
         const credentials = getCredentials();
+        const token = getToken(credentials);
+        token
+            .then(res => {
+                console.log(res);
+                getAllAccounts(token)
+                    .then(res => {
+                        console.log(res.length);
+                        const jsonContent = JSON.stringify(res);
+                        createFile('recuperados.json', jsonContent);
+                    })
+                    .catch(err => console.log('-- ERROR: No se pudo realizar la llamada multiple: ' + err));                
+            })
+            .catch(err => console.error(err));
 
     } catch (error) {
         console.error(error.message);
